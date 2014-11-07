9f69ab08f6940b42e46a8860e6d2e12591d0e22a
Lê Hoàng Phương <herophuong93@gmail.com>
2014-11-07 14:26:35 +0700

    add layout for displaying dota 2 hero

---
+++ tmpl/hero.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_tags_popular
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+?>
+<?php JLoader::register('TagsHelperRoute', JPATH_BASE . '/components/com_tags/helpers/route.php'); ?>
+<div class="tagssimilar<?php echo $moduleclass_sfx; ?>">
+<?php if ($list) : ?>
+    <div class="row">
+    <?php foreach ($list as $i => $item) : ?>
+        <?php
+            $link = JRoute::_(TagsHelperRoute::getItemRoute($item->content_item_id, $item->core_alias, $item->core_catid, $item->core_language, $item->type_alias, $item->router));
+            $image_src = JUri::root() . 'media/com_dota2/images/dota2/heroes/' . $item->core_alias . '_hphover.png';
+        ?>
+        <div class="col-md-3">
+            <?php $item->route = new JHelperRoute; ?>
+            <a href="<?php echo $link; ?>" title="<?php echo htmlspecialchars($item->core_title); ?>">
+                <img src="<?php echo $image_src; ?>" alt="<?php echo htmlspecialchars($item->core_title); ?>" class="img-responsive img-thumbnail" />
+            </a>
+        </div>
+    <?php endforeach; ?>
+    </div>
+<?php else : ?>
+    <span><?php echo JText::_('MOD_TAGS_SIMILAR_NO_MATCHING_TAGS'); ?></span>
+<?php endif; ?>
+</div>
